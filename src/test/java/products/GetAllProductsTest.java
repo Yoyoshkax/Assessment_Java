820f17789a660eb12ef9cd73d9d5ef1aceef63ce
Yoyoshka <Yoyoshkalol@gmail.com>
2026-06-12 10:28:55 +0300

    wip

---
+++ src/test/java/products/GetAllProductsTest.java
@@ -0,0 +1,29 @@
+package products;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GetAllProductsTest {
+
+    ProductDto productDto;
+    ProductClient productClient;
+
+    @BeforeEach
+    public void setUp() {
+        productClient = new ProductClient();
+    }
+
+    @Test
+    @DisplayName("Проверяем что список не пустой и содержить хотя бы один продукт")
+    public void checkListShouldBeNotEmpty() {
+        Response response = productClient.getAllProducts();
+        List<Object> productsList = response.jsonPath().get();
+        assertTrue(!productsList.isEmpty());
+    }
+}

e8e1f941edbaa61c2ab0ca6780b768729dbb734c
Yoyoshka <Yoyoshkalol@gmail.com>
2026-07-04 12:43:11 +0300

    wip

--- src/test/java/categories/CreateCategoryTest.java
+++ src/test/java/categories/CreateCategoryTest.java
@@ -1,6 +1,14 @@
 package categories;
 
+import data.DataGenerator;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CreateCategoryTest {
 
@@ -9,5 +17,27 @@ public class CreateCategoryTest {
     @BeforeEach
     public void setUp() {
         categoriesClient = new CategoriesClient();
+    }
+
+    @Test
+    @DisplayName("Создание новой категории c уникальным айди")
+    public void createNewUniqueCategory() {
+        Response categoryListResponse = categoriesClient.getAllCategory();
+        List<Integer> uniqueId = categoryListResponse.jsonPath().getList("id");
+
+        Response newCategoryResponse = categoriesClient.createCategory(DataGenerator.createUniqueCategory());
+        int newCategoryId = newCategoryResponse.jsonPath().get("id");
+
+        assertFalse(uniqueId.contains(newCategoryId),"Создалась категория с существующим айди");
+
+    }
+
+    @Test
+    @DisplayName("Проверка наличия правильных аттрибутов в новой созданной категории")
+    public void checkCorrectCategoryAttributes() {
+        Response response = categoriesClient.createCategory(DataGenerator.createUniqueCategory());
+        String expectedCategoryName = response.jsonPath().get("name");
+        String imageUrl = response.jsonPath().get("image");
+
     }
 }
---
+++ src/test/java/categories/GetAllCategoriesTest.java
@@ -0,0 +1,26 @@
+package categories;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetAllCategoriesTest {
+
+    CategoriesClient categoriesClient;
+
+    @BeforeEach
+    public void setUp() {
+        categoriesClient = new CategoriesClient();
+    }
+
+    @Test
+    @DisplayName("Список всех категорий")
+    public void getAllExistCategories() {
+        Response response = categoriesClient.getAllCategory();
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode, "Список категорий не получен");
+    }
+}

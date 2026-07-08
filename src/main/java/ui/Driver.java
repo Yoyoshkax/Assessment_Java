0d4160cea181b85d745b50e38c533b95013f76f1
Yoyoshka <Yoyoshkalol@gmail.com>
2026-07-08 17:51:46 +0300

    add webDriver

---
+++ src/main/java/ui/Driver.java
@@ -0,0 +1,34 @@
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.BeforeEach;
+
+public class Driver {
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeEach
+    public void before() {
+        initDriver();
+    }
+
+
+    public void initDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+}
